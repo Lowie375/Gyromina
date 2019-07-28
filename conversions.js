6d9e3467f4d74c36c768e7b2c9aeb1e3dc6a3b17
L375 aka N <40923337+Lowie375@users.noreply.github.com>
2019-07-28 08:11:27 -0700

    0.6.43
    0.6.43 - Added 'conversions.js'.

---
+++ conversions.js
@@ -0,0 +1,54 @@
+module.exports.distance = {
+  ["millimetres", "millimeters", "mm"]: "1000000",
+  ["centimetres", "centimeters", "cm"]: "100000",
+  "inches": "39370.0787401574803150",
+  ["foot", "feet", "ft"]: "3280.8398950131233596",
+  ["yards", "yds"]: "1093.6132983377077865",
+  ["metres", "meters"]: "1000",
+  ["kilometres", "kilometers", "km"]: "1",
+  "miles": "0.6213711922373340",
+  ["nautical_miles", "nauticalmiles", "nmi"]: "0.5399568034557235",
+}
+
+module.exports.time = {
+  "nanoseconds": "604800000000000",
+  "microseconds": "604800000000",
+  "milliseconds": "604800000",
+  ["seconds", "secs"]: "604800",
+  ["minutes", "mins"]: "10080",
+  ["hours", "hrs"]: "168",
+  "days": "7",
+  ["weeks", "wks"]: "1",
+  ["years", "yrs"]: "0.0191780664289865",
+}
+
+module.exports.area = {
+  ["square_metres", "squaremetres", "m^2"]: "0",
+}
+
+module.exports.volume = {
+  ["millilitres", "milliliters", "ml"]: "1000",
+  ["litres", "liters"]: "1",
+}
+
+module.exports.angles = {
+  "gradians": "200",
+  "degrees": "180",
+  "radians": "3.1415926535897932",
+}
+
+module.exports.mass = {
+  "grams": "0",
+}
+
+module.exports.pressure = {
+  "pascals": "0",
+}
+
+module.exports.energy = {
+  "watts": "0",
+}
+
+module.exports.power = {
+  "joules": "0",
+}

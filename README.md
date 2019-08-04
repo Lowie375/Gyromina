df5fee272cec0a7a83384a3fe646cb500a9fe7f3
L375 aka N <40923337+Lowie375@users.noreply.github.com>
2019-08-04 13:36:48 -0700

    0.6.61
    0.6.61 - Updated the 'info' command.

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ L375's experimental bot.
 
 Created by L375#6740, with support from Alten#4148
 
-> Latest stable build: 0.6.6
+> Latest stable build: 0.6.61
 
 ***
 
-Info Pages: https://lx375.weebly.com/Gyromina
+[Info Pages](https://lx375.weebly.com/Gyromina) / [Invite Link](https://discordapp.com/oauth2/authorize?client_id=490590334758420481&permissions=1141234752&scope=bot)
+
--- commands/info.js
+++ commands/info.js
@@ -8,10 +8,11 @@ module.exports.run = {
       .setAuthor("Bot Information", message.client.user.avatarURL)
       .setColor(0x7effaf)
       .setTitle(botinfo.description)
-      .setDescription("~ Created by L375#6740, with support from Alten#4148\n~ Currently running version " + botinfo.version + " on " + message.client.guilds.size + " servers")
+      .setDescription("\• Created by <@334335706740686849>, with support from <@184061887212814336>\n\• Currently running version " + botinfo.version + " on " + message.client.guilds.size + " servers")
       .setFooter("Requested by " + message.author.tag, message.author.avatarURL)
-      .addField("Gyromina Info Pages", "[https://lx375.weebly.com/Gyromina](https://lx375.weebly.com/gyromina)")
-      .addField("Github Repository", "[https://github.com/Lowie375/Gyromina](https://github.com/Lowie375/Gyromina)");
+      .addField("Gyromina Info Pages", "\• [https://lx375.weebly.com/Gyromina](https://lx375.weebly.com/gyromina)")
+      .addField("Invite Link", "\• [https://discord.now.sh/490590334758420481?p1141234752](https://discord.now.sh/490590334758420481?p1141234752)")
+      .addField("Github Repository", "\• [https://github.com/Lowie375/Gyromina](https://github.com/Lowie375/Gyromina)");
 
     message.channel.send({embed});
   },

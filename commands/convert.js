48cc73de182206cb9dba81fb2d8e18b075312049
L375 aka N <40923337+Lowie375@users.noreply.github.com>
2020-02-07 11:18:55 -0800

    0.11.0 (b10)
    0.11.0 (b10) - Began reworking 'convert.js' again.

--- commands/convert.js
+++ commands/convert.js
@@ -1,45 +1,55 @@
 const Discord = require('discord.js');
 
-// Array V3: names[array#][object#] (10 per line) --> converterV3[array#][object#] (0-9/10-19/20-29/etc.)
+// Array V4: names[array#][object#] + metrics[array#][object#] --> converterV3[array#][object#] (0-9/10-19/20-29/etc.)
 const names = [
-  ["millimetres", "millimeters", "mm", "centimetres", "centimeters", "cm", "inches", "in", "foot", "feet",
-   "ft", "yards", "yds", "metres", "meters", "kilometres", "kilometers", "km", "miles", "mi",
-   "nauticalmiles", "nautical_miles", "nmi", "nanoseconds", "nanosecs", "microseconds", "microsecs", "milliseconds", "millisecs", "seconds",
-   "secs", "minutes", "mins", "hours", "hrs", "days", "weeks", "wks", "years", "yrs",
-   "gradians", "grads", "degrees", "degs", "radians", "rads", "milliradians", "millirads", "millilitres", "milliliters",
-   "ml", "litres", "liters", "cubicmetres", "cubicmeters", "cubic_metres", "cubic_meters", "m", "gon", "d",
-   "nsecs", "μsecs", "msecs", "mil", "m³", "m3", "m^3", "usecs", "cm³", "cm3",
-   "cm^3", "cubiccentimetres", "cubiccentimeters", "cubic_centimetres", "cubic_centimeters", "in³", "in3", "in^3", "cubicinches", "cubic_inches",
-   "ft³", "ft3", "ft^3", "cubic_foot", "cubicfoot", "cubic_feet", "cubicfeet", "gallons", "usgallons", "us_gallons",
-   "gal", "usgal", "us_gal", "quarts", "usquarts", "us_quarts", "qt", "usqt", "us_qt", "fluidounces",
-   "fluid_ounces", "floz", "fl_oz", "usfluidounces", "usfloz", "us_fluidounces", "us_floz", "us_fluid_ounces", "us_fl_oz", "pints",
-   "uspints", "us_pints", "pt", "uspt", "us_pt", "tablespoon", "ustablespoon", "us_tablespoon", "tbsp", "ustbsp",
-   "us_tbsp", "teaspoon", "usteaspoon", "us_teaspoon", "tsp", "ustsp", "us_tsp",],
-  ["d0", "d0", "d0", "d1", "d1", "d1", "d2", "d2", "d3", "d3",
-   "d3", "d4", "d4", "d5", "d5", "d6", "d6", "d6", "d7", "d7",
-   "d8", "d8", "d8", "t9", "t9", "t10", "t10", "t11", "t11", "t12", 
-   "t12", "t13", "t13", "t14", "t14", "t15", "t16", "t16", "t17", "t17",
-   "n18", "n18", "n19", "n19", "n20", "n20", "n21", "n21", "v22", "v22",
-   "v22", "v23", "v23", "v24", "v24", "v24", "v24", "d5", "n18", "t15",
-   "t9", "t10", "t11", "n21", "v24", "v24", "v24", "t10", "v22", "v22",
-   "v22", "v22", "v22", "v22", "v22", "v25", "v25", "v25", "v25", "v25",
-   "v26", "v26", "v26", "v26", "v26", "v26", "v26", "v27", "v27", "v27",
-   "v27", "v27", "v27", "v28", "v28", "v28", "v28", "v28", "v28", "v29",
-   "v29", "v29", "v29", "v29", "v29", "v29", "v29", "v29", "v29", "v30",
-   "v30", "v30", "v30", "v30", "v30", "v31", "v31", "v31", "v31", "v31",
-   "v31", "v32", "v32", "v32", "v32", "v32", "v32",]
+  ["metres", "meters", "m", "inches", "in", "foot", "feet", "ft", "yards", "yds",
+    "miles", "mi", "nauticalmiles", "nmi", "seconds", "secs", "s", "minutes", "mins", "hours",
+    "hrs", "days", "d", "weeks", "wks", "years", "yrs", "gradians", "grads", "gon",
+    "degrees", "degs", "°", "radians", "rads", "milliradians", "millirads", "mil", "litres", "liters",
+    "L", "cubicmetres", "cubicmeters", "m³", "m3", "m^3", "in³", "in3", "in^3", "cubicinches",
+    "ft³", "ft3", "ft^3", "cubicfoot", "cubicfeet", "gallons", "usgallons", "gallonsus", "gallonus", "gal",
+    "usgal", "galus", "quarts", "usquarts", "quartsus", "quartus", "qt", "usqt", "qtus", "fluidounces",
+    "floz", "usfluidounces", "usfloz", "fluidouncesus", "fluidounceus", "flozus", "pints", "uspints", "pt", "uspt",
+    "pintsus", "pintus", "ptus", "tablespoons", "ustablespoons", "tbsp", "ustbsp", "tablespoonsus", "tablespoonus","tbspus",
+    "teaspoons", "usteaspoons", "tsp", "ustsp", "teaspoonsus", "teaspoonus", "tspus", "\"", "\'"
+    ], // GOOD!
+    // WIP below:
+  ["d0", "d0", "d0", "d1", "d1", "d2", "d2", "d2", "d3", "d3",
+    "d4", "d4", "d5", "d5", "t6", "t6", "t6", "t7", "t7", "t8",
+    "t8", "t9", "t9", "t10", "t10", "t11", "t11", // good until here
+    
+    "n12", 
+    "t12", "t13", "t13", "t14", "t14", "t15", "t16", "t16", "t17", "t17",
+    "n18", "n18", "n19", "n19", "n20", "n20", "n21", "n21", "v22", "v22",
+    "v22", "v23", "v23", "v24", "v24", "v24", "v24", "d5", "n18", "t15",
+    "t9", "t10", "t11", "n21", "v24", "v24", "v24", "t10", "v22", "v22",
+    "v22", "v22", "v22", "v22", "v22", "v25", "v25", "v25", "v25", "v25",
+    "v26", "v26", "v26", "v26", "v26", "v26", "v26", "v27", "v27", "v27",
+    "v27", "v27", "v27", "v28", "v28", "v28", "v28", "v28", "v28", "v29",
+    "v29", "v29", "v29", "v29", "v29", "v29", "v29", "v29", "v29", "v30",
+    "v30", "v30", "v30", "v30", "v30", "v31", "v31", "v31", "v31", "v31",
+    "v31", "v32", "v32", "v32", "v32", "v32", "v32",]
 ]; // d=dist // t=time // n=angles // v=vol // p=pressure // a=area // e=energy // m=mass // w=power // g=weight //
 
 const converter = [
-  ["mm", "cm", "in", "ft", " yds", "m", "km", "mi", "nmi", " nsec",
-   " μsec", " msec", " sec", " min", " hrs", " days", " wks", " yrs", " gon", "°",
-   " rads", " mil", "mL", "L", "m³", "in³", "ft³", " US gal", " US qt", " US fl oz",
-   " US pt", " US tbsp", " US tsp"],
+  ["m", "in", "ft", " yds", "mi", "nmi", "/sec", " min", " hrs", " days",
+    " wks", " yrs", " gon", "°", " rads", " mil", "L", "m³", "in³", "ft³",
+    " US gal", " US qt", " US floz", " US pt", " US tbsp", " US tsp"], // GOOD!
+  // WIP below:
   [1609344, 160934.4, 63360, 5280, 1760, 1609.344, 1.609344, 1, 1609.344/1852, 604800000000000,
-   604800000000, 604800000, 604800, 10080, 168, 7, 1, 0.0191780664289865, 200, 180,
-   "π", "π/1000", 1000, 1, 0.001, 1/0.016387064, 1/28.316846592, 1/3.785411784, 4/3.785411784, 128/3.785411784,
-   8/3.785411784, 256/3.785411784, 768/3.785411784]
+    604800000000, 604800000, 604800, 10080, 168, 7, 1, 0.0191780664289865, 200, 180,
+    "π", "π/1000", 1000, 1, 0.001, 1/0.016387064, 1/28.316846592, 1/3.785411784, 4/3.785411784, 128/3.785411784,
+    8/3.785411784, 256/3.785411784, 768/3.785411784]
 ];
+
+function metricCases(x) {
+  return x;
+}
+
+function cleanArgs(args) {
+  let cleaned = [];
+  return args;
+}
 
 function valCases(x) {
   var result = x;
@@ -111,10 +121,13 @@ module.exports.run = {
       case 1: message.reply("I can\'t convert something if you don't tell me the unit it's in!"); return;
       case 2: message.reply("I can\'t convert something if you don't tell me what unit to convert it to!"); return;
     }
+    
+    var cArgs = cleanArgs(args);
+    
     // Finds & pulls conversion data
-    var pos1 = search(args, 1);
+    var pos1 = search(cArgs, 1);
     if (errorPull(pos1, message) != 0) return;
-    var pos2 = search(args, 2);
+    var pos2 = search(cArgs, 2);
     if (errorPull(pos2, message) != 0) return;
 
     var type1 = pos1.slice(0, 1);
@@ -127,8 +140,8 @@ module.exports.run = {
       message.reply("I can\'t convert between 2 unlike units! Please check your units and try again.");
       return;
     }
-    var name1 = nameCases(converter[0][pos1], args);
-    var name2 = nameCases(converter[0][pos2], args);
+    var name1 = nameCases(converter[0][pos1], cArgs);
+    var name2 = nameCases(converter[0][pos2], cArgs);
 
     // Initialises conversion values
     var val1, val2, output, round;
@@ -146,28 +159,28 @@ module.exports.run = {
     }
 
     // Converts and checks if the output is valid
-    output = args[0] * val2 / val1;
+    output = cArgs[0] * val2 / val1;
     if (isNaN(output) == 1) {
       message.reply("I can't convert non-numerical values! Please enter a valid number and try again.");
       return;
     }
 
     // Creates an approximation to go alongside the full conversion, if necessary
-    if(!args[4] || args[4] < 0) {
+    if(!cArgs[4] || cArgs[4] < 0) {
       round = Math.round(output*10)/10;
-    } else if (args[4] == 0) {
+    } else if (cArgs[4] == 0) {
       round = "null";
       output = Math.round(output);
     } else {
       round = Math.round(output);
-      output = Math.round(output*Math.pow(10, args[4]))/Math.pow(10, args[4]);
+      output = Math.round(output*Math.pow(10, cArgs[4]))/Math.pow(10, cArgs[4]);
     }
     if(round == output && output % 1 != 0) round = Math.round(output);
     if(round == output && output % 1 == 0) round = "null";
 
     // Creates and sends the embed
     const embed = new Discord.RichEmbed()
-      .setTitle(`${args[0]}${name1} equals…\n\`${output}${name2}\``)
+      .setTitle(`${cArgs[0]}${name1} equals…\n\`${output}${name2}\``)
       .setColor(0x7effaf);
 
     if(round != "null") embed.setDescription(`…or about ${round}${name2}`);
@@ -183,6 +196,6 @@ module.exports.help = {
   "usage": `${process.env.prefix}convert <value> <unit> <newUnit> [places]`,
   "params": "<value> <unit> <newUnit> [places]",
   "hide": 0,
-  "wip": 0,
+  "wip": 1,
   "dead": 0,
 };

e6ea8184c47dbec719c41fe1bbe8aca5d75e45f1
L375 aka N <40923337+Lowie375@users.noreply.github.com>
2020-04-09 23:32:09 -0700

    0.11.0 (b18)
    0.11.0 (b18) - Tweaked a few things, including the 'info' command and the 'README' file.

--- commands/info.js
+++ commands/info.js
@@ -7,14 +7,14 @@ module.exports.run = {
     const embed = new Discord.MessageEmbed()
       .setAuthor("The Gyroscopic Dictionary", client.user.avatarURL())
       .setColor(0x7effaf)
-      .setTitle("Gy·ro·min·a\n/ˈjīräminə/")
-      .setDescription(`*noun:*\n[1] A multipurpose Discord bot created by <@${botinfo.authorID}>, with contributions & support from <@${botinfo.contributorIDs[0]}>, currently running version ${botinfo.version} on ${client.guilds.size} servers\n[2] \"${botinfo.tagline}"\"`)
+      .setTitle("Gy·ro·mi·na\n/ˈjīräminə/")
+      .setDescription(`*noun:*\n[1] A multipurpose Discord bot created by <@${botinfo.authorID}>, with contributions & support from <@${botinfo.contributorIDs[0]}>, currently running version ${botinfo.version} on ${client.guilds.cache.size} servers\n[2] \"${botinfo.tagline}\"`)
       .setFooter(`Requested by ${message.author.tag} / Source: package.json`, message.author.avatarURL())
       .setTimestamp()
-      .addField("*Origin*", "​*More information: [https://lx375.weebly.com/gyromina](https://lx375.weebly.com/gyromina)*")
+      .addField("*Origin*", "​***\`    L-V3R7     --->    Gyromina   \`***\n*\` Jul 07, 2018        Sep 20, 2018 \`\nMore information: [https://lx375.weebly.com/gyromina](https://lx375.weebly.com/gyromina)*")
       .addField("*Usage*", "*\"You can invite Gyromina to your Discord server using this link: [https://discord.now.sh/490590334758420481?p1141234752](https://discord.now.sh/490590334758420481?p1141234752)\"*")
-      .addField("*See also*", "​*Github repository: [https://github.com/Lowie375/Gyromina](https://github.com/Lowie375/Gyromina)\n​[Project Manager \(Github\)](https://github.com/Lowie375/Gyromina/projects/1)"
-        + " / [Report a Bug \(Github\)](https://github.com/Lowie375/Gyromina/issues) / [Bug Tracker \(Github\)](https://github.com/Lowie375/Gyromina/projects/2)*")
+      .addField("*See also*", "​*Github repository: [https://github.com/Lowie375/Gyromina](https://github.com/Lowie375/Gyromina)\n​[Project Manager](https://github.com/Lowie375/Gyromina/projects/1)"
+        + " / [Report a Bug](https://github.com/Lowie375/Gyromina/issues) / [Bug Tracker](https://github.com/Lowie375/Gyromina/projects/2)*")
       .setThumbnail('https://cdn.discordapp.com/avatars/490590334758420481/5630ed9ee4b27498147e569ac52641f1.png')
 
     message.channel.send({embed: embed});

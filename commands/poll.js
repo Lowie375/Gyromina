9a758c481ec977fe9d7e5a3c4506af93b37814e5
L375 aka N <40923337+Lowie375@users.noreply.github.com>
2020-04-24 20:58:35 -0700

    0.12.0 (b3; reupload)
    * Made a bunch of miscellaneous tweaks (and FINALLY uploaded the RIGHT CODE to the RIGHT BRANCH. I could not catch a break today.)

--- commands/poll.js
+++ commands/poll.js
@@ -1,12 +1,38 @@
 module.exports.run = {
+  execute(message, args, client) {
 
+    // Pseudocode time!
+      // check if poll is valid (has type/prompt/options/etc.)
+      // create embed shell
+      // check poll type
+      // CASE 1: preset poll
+        // add prompt
+        // react with options (await)
+      // CASE 2: custom poll
+        // add prompt
+        // handle emojis + descriptors
+        // METHOD I:
+          // join array + .split("-") + .shift()
+          // .split(" ") to temp array
+          // join + push to final array
+        // METHOD II:
+          // detect using .startsWith("-")
+          // split off to temp array
+          // join + push to final array
+        // add emojis + descriptors
+        // reach with options (await)
+      // post poll embed
+      // done!
+    
+  },
 };
 
 module.exports.help = {
   "name": "poll",
-  "description": "Creates a poll. Type \'yn\' creates a yes/no poll, and type \'ynn\' creates a yes/neutral/no poll. Custom polls can be made using the 2nd syntax (e# = emoji, o# = option text), with 1-20 options per poll.",
-  "usage": `${process.env.prefix}poll <type> <prompt> \n   **--OR--** ${process.env.prefix}poll <prompt> -<e1> <o1> -[e2] [o2] …`,
-  "params": ["<type> <prompt>", "<prompt> -<e1> <o1> -[e2] [o2] …"],
+  "description": "Creates a poll in the current channel.",
+  "usage": `${process.env.prefix}poll <type> <prompt> -[options]\n   **--OR--** ${process.env.prefix}poll <prompt> -<e1> [o1] -[e2] [o2] …`,
+  "params": ["<type> <prompt> -[options]", "<prompt> -<e1> [o1] -[e2] [o2] …"],
+  "helpurl": "https://lx375.weebly.com/gyrocmd-poll",
   "hide": 0,
   "wip": 1,
   "dead": 0,
--- commands/shutdown.js
+++ commands/shutdown.js
@@ -3,30 +3,33 @@ const package = require('../package.json');
 const e = require('../systemFiles/emojis.json');
 
 module.exports.run = {
-  execute(message, args, client) {
+  async execute(message, args, client) {
     // Emoji setup
     const nope = client.emojis.cache.get(e.nope);
     const yep = client.emojis.cache.get(e.yep);
     
     // Checks to see if the bot owner (L375#6740) sent the message.
-    if(message.author.id !== package.authorID) {
+    if(message.author.id !== package.hostID) {
       message.channel.send(`${nope} Error - Insufficient permissions!`)
       console.log('A user attempted to shut me down, but was unsuccessful!')
       return;
     }
 
     // Shuts down the current instance of the Discord Client.
-    message.channel.send(`${yep}`);
+    let tag = client.user.tag;
+    await message.channel.send(`${yep}`);
     client.user.setStatus("invisible");
-    console.log(`Shutting down ${client.user.tag}...\n- - - - - - - - - - -`);
-    client.destroy();
+    console.log(`Shutting down ${tag}...`);
+    await client.destroy();
+    let t = new Date();
+    console.log(`Successfully shut down ${tag} on ${t.toUTCString()}.\n- - - - - - - - - - -`)
   },
 };
 
 module.exports.help = {
   "name": "shutdown",
-  "aliases": ["off", "stop", "quit"],
-  "description": "Shuts down Gyromina. (Owner only)",
+  "aliases": ["off", "stop", "quit", "shutoff"],
+  "description": "Shuts down the current instance of Gyromina. (Owner only)",
   "usage": `${process.env.prefix}shutdown`,
   "params": "(owner only)",
   "hide": 1,

21fc37636679b23f6c950846f30eff647b3393fd
Nanjou <lowie375@gmail.com>
2021-08-13 17:11:14 -0700

    1.1.9-pre.13
    * Reverted an incompatible change from 1.2.0-indev

--- commands/fraction.js
+++ commands/fraction.js
@@ -318,9 +318,9 @@ exports.run = {
     if(set[0] == "x") {
       switch(results[0]) {
         case "t": // terminating
-          return message.reply(`I think this is a terminating decimal. If I'm wrong, try this command again with a **\`-r\`** at the end.\n${flow[getRandomInt(0, flow.length-1)]} here you go!`, {embed: embed});
+          return message.channel.send(`I think this is a terminating decimalm, <@${message.author.id}>. If I'm wrong, try this command again with a **\`-r\`** at the end.\n${flow[getRandomInt(0, flow.length-1)]} here you go!`, {embed: embed});
         default: // repeating
-          return message.reply(`I think this is a repeating decimal. If I'm wrong, try this command again with a **\`-t\`** at the end.\n${flow[getRandomInt(0, flow.length-1)]} here you go!`, {embed: embed});
+          return message.channel.send(`I think this is a repeating decimal, <@${message.author.id}>. If I'm wrong, try this command again with a **\`-t\`** at the end.\n${flow[getRandomInt(0, flow.length-1)]} here you go!`, {embed: embed});
       }
     } else {
       return message.channel.send(`Here you go, <@${message.author.id}>!`, {embed: embed});
